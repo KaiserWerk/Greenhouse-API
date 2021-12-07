0bff6c31ce5579cc815a274840caa879c7ddac66
Robin Kaiser <web@r-k.mx>
2021-12-07 18:46:15 +0100

    Update api.go

--- internal/handler/api.go
+++ internal/handler/api.go
@@ -1,7 +1,20 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/KaiserWerk/Greenhouse-Manager/internal/entity"
+	"github.com/KaiserWerk/Greenhouse-Manager/internal/influx"
+)
 
 func (h HttpHandler) ReceiveHandler(w http.ResponseWriter, r *http.Request) {
+	var m entity.Measurement
+	if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
+		http.Error(w, "could not decode JSON", http.StatusBadRequest)
+	}
+	_ = r.Body.Close()
 
+	influx.InsertMeasurement(m)
+	w.WriteHeader(http.StatusCreated)
 }
