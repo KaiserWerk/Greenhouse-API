e921aa79e14ef5a4d84166283d6e3a76dde96929
Robin Kaiser <web@r-k.mx>
2021-12-07 18:46:05 +0100

    Update auth.go

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -3,12 +3,14 @@ package middleware
 import (
 	"net/http"
 	"os"
+
+	"github.com/KaiserWerk/Greenhouse-Manager/internal/config"
 )
 
 func Auth(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		envKey := os.Getenv("GREENHOUSE_KEY")
-		headerKey := r.Header.Get("X-Greenhouse-Key")
+		envKey := os.Getenv(config.EnvKey)
+		headerKey := r.Header.Get(config.HeaderKey)
 
 		if envKey == "" || headerKey == "" {
 			http.Error(w, "empty or missing key", http.StatusUnauthorized)
